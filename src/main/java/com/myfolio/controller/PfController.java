2f7c8e5a11a06b5928a4820970803b5b29b25028
Akhil <akhilselukar.work@gmail.com>
2022-02-11 19:09:08 +0530

    transferred all Html/css code to maven project.

--- src/main/java/com/myfolio/controller/PfController.java
+++ src/main/java/com/myfolio/controller/PfController.java
@@ -1,15 +1,24 @@
 package com.myfolio.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PfController {
 	
+	// inject via application.properties
+	@Value("${home.fName:test}")
+	private String fName;
+	@Value("${home.lName:test}")
+	private String lName;
+	
 	@RequestMapping("/")
-	/*public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);*/
-	public String welcome() {
+	public String welcome(Map<String, Object> model) {
+		model.put("fName", this.fName);
+		model.put("lName", this.lName);
 		return "home";
 	}
 }
